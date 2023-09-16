16767fb24a62c78b0948ca2ee9f18bb4a3294e48
MPXΣ² <69911493+MPX0222@users.noreply.github.com>
2023-09-16 11:45:00 +0800

    Add files via upload

---
+++ BroadLearningSystemBasedAutoEncoder.py
@@ -0,0 +1,64 @@
+from BoradLearningSystem import *
+
+class BLSAutoEncoder(BLS):
+    def __init__(self, NumFeatureNodes=10, NumWindows=10, NumEnhance=10, S=0.5, C=2 ** -30, NumBlock=3):
+        super().__init__()
+
+
+class BLSAEExtractor(BLSAutoEncoder):
+    def __init__(self, NumFeatureNodes=10, NumWindows=10, NumEnhance=10, S=0.5, C=2 ** -30, NumBlock=3):
+        super().__init__()
+
+    def fit(self, train_x, train_y):
+        # --Train--
+        train_x = preprocessing.scale(train_x, axis=1)  # 标准化处理样本
+        Feature_InputDataWithBias = np.hstack([train_x, 0.1 * np.ones((train_x.shape[0], 1))])  # 将输入矩阵进行行链接，即平铺展开整个矩阵
+        Output_FeatureMappingLayer = np.zeros([train_x.shape[0], self.FeatureWindows * self.FeatureNodes])
+
+        self.Beta1_EachWindow = []
+        self.Dist_MaxAndMin = []
+        self.Min_EachWindow = []
+        self.ymin = 0
+        self.ymax = 1
+
+        # 特征层
+        for i in range(self.FeatureWindows):
+            random.seed(i + 2022)
+            W_EachWindow = 2 * random.randn(train_x.shape[1] + 1, self.FeatureNodes) - 1  # 随机化特征层初始权重
+            Feature_EachWindow = np.dot(Feature_InputDataWithBias, W_EachWindow)  # 计算每个特征映射中间态
+
+            # scaler1 = preprocessing.MinMaxScaler(feature_range=(0, 1)).fit(Feature_EachWindow)                      # 对上述结果归一化处理
+            # Feature_EachWindowAfterPreprocess = scaler1.transform(Feature_EachWindow)                               # 进行标准化
+
+            Feature_EachWindowAfterPreprocess = Feature_EachWindow  # 进行标准化
+            Beta_EachWindow = self.sparse_bls(Feature_EachWindowAfterPreprocess, Feature_InputDataWithBias).T  # 随机化特征映射初始偏置
+            self.Beta1_EachWindow.append(Beta_EachWindow)
+            Output_EachWindow = np.dot(Feature_InputDataWithBias, Beta_EachWindow)  # 计算每个特征映射最终输出
+
+            self.Dist_MaxAndMin.append(np.max(Output_EachWindow, axis=0) - np.min(Output_EachWindow, axis=0))  # 计算损失函数
+            self.Min_EachWindow.append(np.min(Output_EachWindow, axis=0))
+            Output_EachWindow = (Output_EachWindow - self.Min_EachWindow[i]) / self.Dist_MaxAndMin[i]
+            # 计算特征层最终输出
+            Output_FeatureMappingLayer[:, self.FeatureNodes * i:self.FeatureNodes * (i + 1)] = Output_EachWindow
+
+        # 增强层
+        train_ori_hance = np.hstack([train_x])
+        train_x_enhance = preprocessing.scale(train_ori_hance, axis=1)
+        Input_EnhanceLayerWithBias = np.hstack([train_x_enhance, 0.1 * np.ones((train_x_enhance.shape[0], 1))])
+
+        self.W_EnhanceLayer = LA.orth(2 * random.randn(train_x_enhance.shape[1] + 1, self.EnhancementNodes)) - 1
+
+        Temp_Output_EnhanceLayer = np.dot(Input_EnhanceLayerWithBias, self.W_EnhanceLayer)  # 计算增强层中间态
+        self.Parameter_Shrink = self.S / np.max(Temp_Output_EnhanceLayer)
+        Output_EnhanceLayer = self.relu(Temp_Output_EnhanceLayer * self.Parameter_Shrink)  # 计算增强层最终输出
+
+        # 输出层
+        Input_OutputLayer = np.hstack([Output_FeatureMappingLayer, Output_EnhanceLayer])  # 合并特征层和增强层作为输出层输入
+        Pinv_Output = self.cls_pinv(Input_OutputLayer)  # 计算伪逆
+
+        # 计算系统总权重
+        self.W = np.dot(Pinv_Output, train_x)
+
+        OutputFeatureX = np.dot(train_x, self.W.T)  # 计算预测输出
+
+        return OutputFeatureX
--- test_demo.py
+++ test_demo.py
@@ -1,9 +1,14 @@
-from BoradLearningSystem import *
+from BoradLearningSystem import BLSRegressor, BLSClassifier
+from BroadLearningSystemBasedAutoEncoder import BLSAEExtractor
 from sklearn.datasets import load_iris, load_breast_cancer
 
-regressor = BLSRegressor(NumFeatureNodes=10, NumWindows=10, NumEnhance=10, S=0.5, C=2**-30)
-classifier = BLSClassifier()
+# -- 模型初始化 --
+# regressor = BLSRegressor(NumFeatureNodes=10, NumWindows=10, NumEnhance=10, S=0.5, C=2**-30)
+# classifier = BLSClassifier()
+extractor = BLSAEExtractor()
 
+
+# -- mat文件 加载minst --
 # dataFile = 'code/mnist.mat'
 # data = scio.loadmat(dataFile)
 # traindata = np.double(data['train_x']/255)
@@ -11,23 +16,30 @@ classifier = BLSClassifier()
 # testdata = np.double(data['test_x']/255)
 # testlabel = np.double(data['test_y'])
 
+
+# -- sklearn 加载回归数据 --
 # dataset = load_iris()
 # data, label = dataset['data'], dataset['target']
 # print(label)
 
+# -- sklearn 加载分类数据 --
 cls_dataset = load_breast_cancer()
 cls_data, cls_label = cls_dataset['data'], cls_dataset['target']
-print(cls_data.shape, cls_label.shape)
-print(cls_label)
+print(cls_data.shape)
 
+# -- BLSAE --
+feature_encode_x = extractor.fit(cls_data, cls_label)
+print(feature_encode_x.shape)
 
+# -- 回归 --
 # train_output = regressor.fit(data, label)
 # print(train_output.shape)
 # predict_output = regressor.predict(data)
 # print(predict_output.shape)
 
-train_output = classifier.fit(cls_data, cls_label, is_excel_label=True)
+# -- 分类 --
+# train_output = classifier.fit(cls_data, cls_label, is_excel_label=True)
 # print(train_output)
-predict_output = classifier.predict(cls_data)
-print(predict_output)
+# predict_output = classifier.predict(cls_data)
+# print(predict_output)
 
